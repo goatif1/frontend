97bb30cc980dfaf786b3f332cee476b22bcfa1c7
xoselolo <1a09lopxose@gmail.com>
2023-09-01 20:55:03 +0200

    load images from inside src/images foldes

--- src/public/components/buttons/AppBarLogoButton.js
+++ src/public/components/buttons/AppBarLogoButton.js
@@ -1,9 +1,10 @@
 import { makeStyles } from "@mui/styles";
 import { useNavigate } from "react-router-dom";
 import { hasToken } from "../../../utils/access";
+import app_logo from "../../../images/app_logo.jpg";
 
-// const APP_LOGO_PATH = "src/assets/images/app_logo.jpg";
-const APP_LOGO_PATH = "./images/app_logo.jpg";
+// const APP_LOGO_PATH = "./images/app_logo.jpg";
+// const APP_LOGO_PATH = "./images/app_logo.jpg";
 
 const useStyles = makeStyles({
     imageButton: {
@@ -24,7 +25,8 @@ const AppBarLogoButton = (props) => {
     return (
         <img
             className={classes.imageButton}
-            src={APP_LOGO_PATH}
+            src={app_logo}
+            // src={APP_LOGO_PATH}
             onClick={() => {
                 if (user_is_logged){
                     navigate("/home");
